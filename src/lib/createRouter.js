921f466e9b2b4a2839285c0863e5e5a02103017b
한수지 <fatholic5@gmail.com>
2025-04-02 03:40:30 +0900

    fix: 디버깅을 위한 콘솔 임시 추가

--- src/lib/createRouter.js
+++ src/lib/createRouter.js
@@ -7,18 +7,20 @@ export const createRouter = (routes) => {
 
   const getPath = () => {
     const fullPath = window.location.pathname;
-
-    return baseUrl === "/"
-      ? fullPath
-      : fullPath.replace(new RegExp(`^${baseUrl}`), "");
+    const path =
+      baseUrl === "/"
+        ? fullPath
+        : fullPath.replace(new RegExp(`^${baseUrl}`), "");
+    console.log("Current Path:", path); // 현재 경로 출력
+    return path;
   };
 
   const getTarget = () => routes[getPath()];
 
   const push = (path) => {
     const url = baseUrl === "/" ? path : `${baseUrl}${path}`;
-
     window.history.pushState(null, null, url);
+    console.log("Pushed URL:", url); // 푸시된 URL 출력
     notify();
   };
 
